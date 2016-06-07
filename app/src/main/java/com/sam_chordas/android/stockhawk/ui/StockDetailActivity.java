aa48522452c06b15e6b9bbce5f55c5419f581477
Abhishek <abhishek10.rs@gmail.com>
2016-06-08 00:57:14 +0530

    fixed widget

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
@@ -0,0 +1,132 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.db.chart.Tools;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.AxisController;
+import com.db.chart.view.LineChartView;
+import com.db.chart.view.animation.Animation;
+import com.sam_chordas.android.stockhawk.R;
+import com.sam_chordas.android.stockhawk.data.QuoteColumns;
+import com.sam_chordas.android.stockhawk.data.QuoteProvider;
+import com.squareup.picasso.Picasso;
+
+public class StockDetailActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    public static final String TAG_STOCK_SYMBOL = "STOCK_SYMBOL";
+    private static final int STOCKS_LOADER = 1;
+
+    private String currency;
+    private LineChartView lineChartView;
+
+    public static Intent getStartActivityIntent(Context context, String currency) {
+        Intent intent = new Intent(context, StockDetailActivity.class);
+        intent.putExtra(StockDetailActivity.TAG_STOCK_SYMBOL, currency);
+
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_graph);
+
+        currency = getIntent().getStringExtra(TAG_STOCK_SYMBOL);
+
+        setTitle(currency.toUpperCase());
+        lineChartView = (LineChartView) findViewById(R.id.linechart);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportLoaderManager().initLoader(STOCKS_LOADER, null, this);
+
+        StringBuilder urlStringBuilder = new StringBuilder();
+        urlStringBuilder.append("http://chart.finance.yahoo.com/z?s=");
+        urlStringBuilder.append(currency);
+        urlStringBuilder.append("&t=6m&q=l&l=on&z=s&p=m50,m200");
+        String url = urlStringBuilder.toString();
+
+        ImageView iv = (ImageView) findViewById(R.id.graph_image);
+        Picasso.with(this).load(url).resize(720, 360).into(iv);
+
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case STOCKS_LOADER:
+                return new CursorLoader(this, QuoteProvider.Quotes.CONTENT_URI,
+                        new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
+                                QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP},
+                        QuoteColumns.SYMBOL + " = ?",
+                        new String[]{currency},
+                        null);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data.getCount() != 0)
+            renderChart(data);
+
+    }
+
+    public void renderChart(Cursor data) {
+        LineSet lineSet = new LineSet();
+        float minimumPrice = Float.MAX_VALUE;
+        float maximumPrice = Float.MIN_VALUE;
+
+        for (data.moveToFirst(); !data.isAfterLast(); data.moveToNext()) {
+            String label = data.getString(data.getColumnIndexOrThrow(QuoteColumns.BIDPRICE));
+            float price = Float.parseFloat(label);
+
+            lineSet.addPoint(label, price);
+            minimumPrice = Math.min(minimumPrice, price);
+            maximumPrice = Math.max(maximumPrice, price);
+        }
+
+        lineSet.setColor(Color.parseColor("#758cbb"))
+                .setFill(Color.parseColor("#2d374c"))
+                .setDotsColor(Color.parseColor("#758cbb"))
+                .setThickness(4)
+                .setDashed(new float[]{10f, 10f});
+
+
+        lineChartView.setBorderSpacing(Tools.fromDpToPx(15))
+                .setYLabels(AxisController.LabelPosition.OUTSIDE)
+                .setXLabels(AxisController.LabelPosition.NONE)
+                .setLabelsColor(Color.parseColor("#6a84c3"))
+                .setXAxis(false)
+                .setYAxis(false)
+                .setAxisBorderValues(Math.round(Math.max(0f, minimumPrice - 5f)), Math.round(maximumPrice + 5f))
+                .addData(lineSet);
+
+        Animation anim = new Animation();
+
+        if (lineSet.size() > 1)
+            lineChartView.show(anim);
+        else
+            Toast.makeText(this, "No data", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+    }
+}
